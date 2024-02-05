c2a9fd51e4ba65d0c055da83e87ec4b3474fc12b
OthmaneDevLab <adamlamzaouak987@gmail.com>
2024-02-05 16:25:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const mobileBtn = document.querySelector("#mobail-btn")
+const mobileMenu = document.querySelector("#mobaile-menu")
+ mobileBtn.addEventListener('click',()=>{
+    mobileMenu.classList.toggle('hidden');
+ })
